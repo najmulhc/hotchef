ee2ef6a9b686e878e4a76c6f49cfbb06428c0008
kridi32 <kridi32@outlook.com>
2022-04-15 16:18:11 +0600

    navbar responsive done

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import logo from "./img/logo.png";
 
 const Header = () => {
-    return (
-        <div>
-            <p>this is the header</p>
-        </div>
-    );
+    const [open, setOpen] = useState(true);
+  return (
+    <header className="shadow-md w-full fixed top-0 left-0 bg-red-400">
+     <div className="md:flex items-center md:justify-between  container md:mx-auto  py-4 ">
+         <div className="flex  items-center">
+             <img src={logo} className='h-12 pl-8 md:pl-0' alt="" />
+         </div>
+         <div className="text-3xl  absolute right-8 top-6 cursor-pointer text-white md:hidden " onClick={()=> setOpen(!open)}>
+         <ion-icon name={open? 'close': 'menu'}></ion-icon>
+         </div>
+         <ul className={`md:flex md:items-center md:gap-4 absolute md:static bg-red-400 md:z-auto z-[-1] left-0 w-full md:w-auto transition-all duration-700 ease-out ${open? 'top-20': 'top-[-490px]'} text-white`}>
+             <li className="md:ml-4 pl-4 md:my-0 my-4 ">
+                 <Link to='/'>Home</Link>
+             </li>
+             <li className="md:ml-4 pl-4  md:my-0 my-4  ">
+                 <Link to='/login'>login</Link>
+             </li>
+         </ul>
+     </div>
+    </header>
+  );
 };
 
 export default Header;
