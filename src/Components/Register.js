adac6428d4bda82517e4d92bf8d2d0f1347b5259
kridi32 <kridi32@outlook.com>
2022-04-16 10:58:33 +0600

    confirm password added on register form

--- src/Components/Register.js
+++ src/Components/Register.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import {useCreateUserWithEmailAndPassword, useUpdateProfile} from 'react-firebase-hooks/auth';
 import {auth} from '../firebase.init'
 import loginBanner from "./img/loginBanner.png";
@@ -8,20 +8,25 @@ const Register = () => {
     const [displayName, setDisplayName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
     const [
         createUserWithEmailAndPassword, user, loading, error
     ] = useCreateUserWithEmailAndPassword(auth);
     const [updateProfile, updating, updateError] = useUpdateProfile(auth);
+    const navigate = useNavigate()
 
     const register = async (e) => {
         e.preventDefault();
         console.log (email, password);
+       if(password === confirmPassword){
         await createUserWithEmailAndPassword(email,password);
         await updateProfile({displayName: displayName});
+       }
         
     }
     if(user){
         console.log(user);
+        navigate('/')
     }
     return (
         <div className=" mt-20  ">
@@ -58,6 +63,13 @@ const Register = () => {
                   className="w-full border rounded-sm border-lal text-xl mb-3 p-2 "
                   placeholder="Enter password"
                   onChange={(e)=> setPassword(e.target.value) }
+                  required
+                />
+                <input
+                  type="password"
+                  className="w-full border rounded-sm border-lal text-xl mb-3 p-2 "
+                  placeholder="Confirm password"
+                  onChange={(e)=> setConfirmPassword(e.target.value) }
                   required
                 />
                 <small className="text-gray-700 m-0 p-0 text-left">
